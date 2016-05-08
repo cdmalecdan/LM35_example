354960d68053ac5a95d9515852a6d3c1e0472dcd
layadcircuits <cdmalecdan@gmail.com>
2016-05-08 14:34:23 +0800

    Added files via upload
    Formula for the LM35 analog temperature sensor

---
+++ LM35_example.ino
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2016 (C) layadcircuits.com
+ * Project: 
+ * Formula for the LM35 analog temperature sensor.
+ * This code assumes the LM35 is hooked to pin A0.
+ * For Questions/Comments: chris@layadcircuits.com
+ * Revision History:
+ * 1.0 - initial commit
+ */
+float temperature;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  temperature = analogRead(A0) * 0.488;
+  Serial.print(temperature);
+  Serial.println(" C");
+  delay(1000);
+}
